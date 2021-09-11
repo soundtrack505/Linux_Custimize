fd00ff00448bef7013465c4587c6e03bffbab4f6
soundtrack <yarom750@gmail.com>
2021-09-11 15:09:22 -0400

    Done

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install python3-pip python3-venv xclip
+
+# Setting up zshrc
+rm ~/.zshrc && cp zshrc ~/.zshrc
+
+# Making opt writable for me
+sudo chown $(whoami):$(whoami) /opt && mkdir /opt/tools
+
+# Installing oh my zsh
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+# Installing ngrok & Setting up ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip ~/Downloads/ngrok.zip && unzip ngrok.zip && mv ngrok /opt/tools && /opt/tools/ngrok authtoken 1y0B7x8qTnxaprDllSzMKQ0SyzQ_7uudCLb3rmgwFiXjMj9cq
+
+#Git installation
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+git clone https://github.com/danielmiessler/SecLists.git /opt/tools/SecLists
+
+git clone https://github.com/SpiderLabs/Responder.git /opt/tools/Responder
+
+git clone https://github.com/openwall/john.git /opt/tools/John
+
+git clone https://github.com/calebstewart/pwncat.git /opt/tools/pwncat
+
+# Installing pwncat on a virtual evn
+python3 -m venv ~/.pwncat-env;
+source ~/.pwncat-env/bin/activate;
+python3 /opt/tools/pwncat/setup.py install;
+
+deactivate
+
+printf "Don't forget to press Prefix+I to download tmux"
