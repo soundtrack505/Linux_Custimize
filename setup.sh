6d489c1dc312655d867b7308d644ea30e17a4b37
Yerom Hemo <77882364+soundtrack505@users.noreply.github.com>
2022-04-07 14:41:06 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && sudo apt update && sudo apt install python3-pip python3-venv xclip exa feroxbuster tmux curl wget python3-dev sqlmap wireshark \
+sudo apt update && curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && sudo apt update && sudo apt install python3-pip python3-venv xclip exa feroxbuster terminator tmux curl wget python3-dev sqlmap wireshark \
        metasploit-framework zsh zsh-autosuggestions zsh-syntax-highlighting nmap docker.io aircrack-ng john powersploit hashcat hydra impacket-scripts crackmapexec powershell-empire exploitdb -y
 
 
