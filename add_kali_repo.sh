8ebee7ba6c3a414466296127f0ece9f9a88f385a
soundtrack505 <77882364+soundtrack505@users.noreply.github.com>
2024-04-18 21:36:23 +0000

    Create add_kali_repo.sh

---
+++ add_kali_repo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6
+echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list"
