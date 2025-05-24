69c8e9925aa4c6691ea2e7002f1ccd174ba737f7
soundtrack505 <77882364+soundtrack505@users.noreply.github.com>
2025-05-24 12:51:38 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -67,6 +67,7 @@ cd ~
 # install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
+cp p10k.zsh ~/.p10k.zsh
 
 
 printf "Open nvim and run :PlugInstall"
